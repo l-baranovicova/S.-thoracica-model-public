75c4374586a5fc6656fe254a4326eca963fe63c9
lenka <lenka@147.251.68.8>
2023-03-17 12:06:19 +0200

    Vectorized

--- initialization.m
+++ initialization.m
@@ -2,9 +2,7 @@ function[pop] = initialization(max_pop)
 
 pop = zeros(max_pop,5);
 %indices: slope, switch point, melanism, size, fitness, decimal for
-%reproduction
 
-for i = 1:max_pop
-    pop(i,1) = 0.5 + rand; %0.5 - 1.5 slope. Slope must be low to get many intermediates
-    pop(i,2) = 6 + rand * 6;     %6-12 switch point
-end
+pop(:,1:2) = rand(max_pop,2) .* [1,6] + [0.5,6];
+%slope = 0.5-1.5
+%switch pt = 6-12
